f061af3fc7333c4e84bfac9daa3ee5c620d1dc28
David Fiel <dave@fiel.us>
2025-08-04 11:32:10 -0400

    added .htaccess copying to dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: './'
+  base: './',
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: '.htaccess', // Path to the source .htaccess file
+          dest: './' // Copy to the root of the dist directory
+        }
+      ]
+    })
+  ]
 });
